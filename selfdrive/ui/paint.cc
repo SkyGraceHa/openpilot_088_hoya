17858416635f3f43294b849b439f2a887f1857b2
Chanho Jung <jungch@naver.com>
2021-08-29 22:54:53 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -121,11 +121,11 @@ static void draw_lead(UIState *s, const cereal::ModelDataV2::LeadDataV3::Reader 
   if (s->scene.radarDistance < 149) {                                         //radar가 인식되면
     // draw_chevron(s, x, y, sz, nvgRGBA(201, 34, 49, fillAlpha), COLOR_ORANGE); //orange ==> red
     // ui_draw_text(s, x, y + sz/1.5f, "R", 20 * 2.5, COLOR_WHITE, "sans-bold");
-    ui_draw_image(s, {x, y, sz, sz*0.13}, "lead_under_radar", 1.0f);  
+    ui_draw_image(s, {x, y, sz, (int) sz * .13}, "lead_under_radar", 1.0f);  
   } else {                                                                                 //camera가 인식되면
     // draw_chevron(s, x, y, sz, nvgRGBA(150, 0, 200, fillAlpha), nvgRGBA(0, 150, 200, 200)); //oceanblue ==> purple
     // ui_draw_text(s, x, y + sz/1.5f, "C", 20 * 2.5, COLOR_WHITE, "sans-bold");
-    ui_draw_image(s, {x, y, sz, sz*0.13}, "lead_under_camera", 1.0f);  
+    ui_draw_image(s, {x, y, sz, (int) sz * .13}, "lead_under_camera", 1.0f);  
   }
 }
 
