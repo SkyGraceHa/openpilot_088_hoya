b9f06d633f0cec4c2383837a569e8d719685a223
Chanho Jung <jungch@naver.com>
2021-09-01 10:18:28 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -527,11 +527,11 @@ static void ui_draw_vision_scc_gap(UIState *s) {
   const int y = 660;  
   
   if(gap <= 0) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_0", 0.3f);}
-  else if (gap == 1) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_1", 0.8f);}
-  else if (gap == 2) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_2", 0.8f);}
-  else if (gap == 3) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_3", 0.8f);}
-  else if (gap == 4) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_4", 0.8f);}
-  else {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_0", 0.8f);}
+  else if (gap == 1) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_1", 0.6f);}
+  else if (gap == 2) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_2", 0.6f);}
+  else if (gap == 3) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_3", 0.6f);}
+  else if (gap == 4) {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_4", 0.6f);}
+  else {ui_draw_image(s, {x, y, w, h}, "lead_car_dist_0", 0.3f);}
 }
 
 static void ui_draw_vision_brake(UIState *s) {
